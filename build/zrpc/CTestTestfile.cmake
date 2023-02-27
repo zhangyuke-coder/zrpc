dce5749ff2f8f0987ef010572b5391663845638f
zhangyuke-coder <1034643857@qq.com>
2023-02-27 22:49:14 +0800

    删除build

--- build/zrpc/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zyk/VcWorkplace/zrpc/zrpc
-# Build directory: /home/zyk/VcWorkplace/zrpc/build/zrpc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("base")
